7cfd038978a549b965cee7de433f5d35c23c99c6
Luis Casas <luisscasas45@gmail.com>
2025-04-11 00:27:45 -0600

    FEAT/  Service Container

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -1,13 +1,11 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
-
+$db = App::resolve(Database::class);
 $currentUserId = 1;
 
-// Fetch the note from the database using the provided ID from the POST request
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_POST['id']
 ])->findOrFail();
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -1,26 +1,17 @@
 <?php
 
-// Import the Database class from the Core namespace
+use Core\App;
 use Core\Database;
 
-// Load the configuration from the config.php file
-$config = require base_path('config.php');
-
-// Create a new instance of the Database class using the database configuration
-$db = new Database($config['database']);
-
-// Simulate the current authenticated user ID (this would normally come from the session)
+$db = App::resolve(Database::class);
 $currentUserId = 1;
 
-// Query the database to fetch the note with the ID passed via GET
 $note = $db->query('select * from notes where id = :id', [
-    'id' => $_GET['id'] // Prevents SQL injection with bound parameters
-])->findOrFail(); // Throws an error if the note is not found
+    'id' => $_GET['id']
+])->findOrFail();
 
-// Check that the current user is the owner of the note
 authorize($note['user_id'] === $currentUserId);
 
-// Render the 'show.view.php' view and pass the necessary data
 view("notes/show.view.php", [
     'heading' => 'Note',
     'note' => $note
