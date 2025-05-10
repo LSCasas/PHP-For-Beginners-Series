d00f1e601f102460a81ea9a73850015365650208
Luis Casas <luisscasas45@gmail.com>
2025-05-10 10:19:05 -0600

    FEAT/ Testing Approaches, Terms, and Considerations

--- Core/Container.php
+++ Core/Container.php
@@ -2,19 +2,24 @@
 
 namespace Core;
 
+use Exception;
+
 class Container
 {
     protected $bindings = [];
-
 
     public function bind($key, $resolver)
     {
         $this->bindings[$key] = $resolver;
     }
+
+    /**
+     * @throws Exception
+     */
     public function resolve($key)
     {
-        if (! array_key_exists($key, $this->bindings)) {
-            throw new \Exception("No matchig binding found for {$key}");
+        if (!array_key_exists($key, $this->bindings)) {
+            throw new Exception("No matching binding found for {$key}");
         }
 
         $resolver = $this->bindings[$key];
